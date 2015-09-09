9ed81e9007d6351590e89c76b0e401992124e1bb
Chinmay Zanpure <czanpure@juniper.net>
2015-09-08 18:28:46 -0700

    Added Test Cases for ClusterListView and ServerListView

---
+++ webroot/setting/sm/ui/test/ui/ClusterListView.test.js
@@ -0,0 +1,80 @@
+define([
+    'co-test-unit',
+    'sm-test-utils',
+    'sm-test-messages',
+    'cluster-list-view-mock-data',
+    'co-grid-contrail-list-model-test-suite',
+    'co-grid-view-test-suite'
+], function (CUnit, smtu, smtm, ClusterListViewMockData, GridListModelTestSuite, GridViewTestSuite) {
+
+    var moduleId = smtm.CLUSTER_LIST_VIEW_COMMON_TEST_MODULE;
+
+    var fakeServerConfig = CUnit.getDefaultFakeServerConfig();
+
+    var fakeServerResponsesConfig = function () {
+        var responses = [];
+
+        responses.push(CUnit.createFakeServerResponse({
+            url: smtu.getRegExForUrl(smwc.URL_TAG_NAMES),
+            body: JSON.stringify(ClusterListViewMockData.getTagNamesData())
+        }));
+
+        responses.push(CUnit.createFakeServerResponse({
+            url: smtu.getRegExForUrl(smwu.getObjectDetailUrl('cluster')),
+            body: JSON.stringify(ClusterListViewMockData.getSingleClusterDetailData())
+        }));
+
+        responses.push(CUnit.createFakeServerResponse({
+            url: smtu.getRegExForUrl('/sm/server/monitoring/config'),
+            body: JSON.stringify(ClusterListViewMockData.getSingleClusterMonitoringConfigData())
+        }));
+
+        responses.push(CUnit.createFakeServerResponse({
+            url: smtu.getRegExForUrl('/sm/server/monitoring/info/summary'),
+            body: JSON.stringify(ClusterListViewMockData.getSingleClusterMonitoringData())
+        }));
+
+        return responses;
+    };
+    fakeServerConfig.getResponsesConfig = fakeServerResponsesConfig;
+
+    var pageConfig = CUnit.getDefaultPageConfig();
+    pageConfig.hashParams = {
+        p: 'setting_sm_clusters'
+    };
+    pageConfig.loadTimeout = 2000;
+
+    var getTestConfig = function () {
+        return {
+            rootView: smPageLoader.smView,
+            tests: [
+                {
+                    viewId: smwl.SM_CLUSTER_GRID_ID,
+                    suites: [
+                        {
+                            class: GridViewTestSuite,
+                            groups: ['all'],
+                            severity: cotc.SEVERITY_LOW
+                        },
+                        {
+                            class: GridListModelTestSuite,
+                            groups: ['all'],
+                            severity: cotc.SEVERITY_LOW,
+                            modelConfig: {
+                                dataGenerator: smtu.commonGridDataGenerator,
+                                dataParsers: {
+                                    gridDataParseFn: smtu.deleteFieldsForClusterScatterChart
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        };
+    };
+
+    var pageTestConfig = CUnit.createPageTestConfig(moduleId, fakeServerConfig, pageConfig, getTestConfig);
+
+    CUnit.startTestRunner(pageTestConfig);
+
+});
